f1e3c1c642d91d5841edc27772626f272cb26732
DespiseMeZXC <2003lesha2003@mail.ru>
2023-01-09 18:37:14 +0300

    add poetry in project

--- trainproject/trainSite/school/serializers.py
+++ trainproject/trainSite/school/serializers.py
@@ -3,15 +3,17 @@ from rest_framework import serializers
 from school.models import *
 
 
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        fields = "__all__"
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
+        fields = "__all__"
+
+class TeacherSerializer(serializers.ModelSerializer):
+    # subject = SubjectSerializer()
+    class Meta:
+        model = Teacher
         fields = "__all__"
 
 
