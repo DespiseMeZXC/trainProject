c7e3ebd7e70f3ccb486b3c39ec7ee8ce49eaad30
DespiseMeZXC <2003lesha2003@mail.ru>
2023-01-15 04:51:52 +0300

    update M2M

--- trainSite/school/models.py
+++ trainSite/school/models.py
@@ -6,10 +6,28 @@ class Teacher(models.Model):
     fio = models.CharField(null=False, max_length=150)
     experience = models.IntegerField(null=False)
     subject = models.ForeignKey("Subject", on_delete=models.PROTECT)
+    student = models.ManyToManyField(
+        to="Student", related_name="student", through="Statement")
 
     def __str__(self):
         return self.fio
 
+
+class Student(models.Model):
+    fio = models.CharField(null=False, max_length=150)
+    grade = models.IntegerField(null=False)
+    teacher = models.ManyToManyField(
+        to="Teacher", related_name="teacher", through="Statement")
+
+    def __str__(self):
+        return self.fio
+
+
+class Statement(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    rating = models.IntegerField(null=False)
+    quarter = models.IntegerField(null=False)
 
 
 class Subject(models.Model):
@@ -18,21 +36,6 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Student(models.Model):
-    fio = models.CharField(null=False, max_length=150)
-    grade = models.IntegerField(null=False)
-
-    def __str__(self):
-        return self.fio
-
-
-class Statement(models.Model):
-    student = models.ManyToManyField(Student, blank=True)
-    teacher = models.ManyToManyField(Teacher, blank=True)
-    rating = models.IntegerField(null=False)
-    quarter = models.IntegerField(null=False) 
 
 
 class Offices(models.Model):
