1d9b62ebf429d5739d1f5d68ca4951705e8a9ace
DespiseMeZXC <2003lesha2003@mail.ru>
2023-01-20 06:47:20 +0300

    maybe done swagger teacher

--- trainSite/school/views.py
+++ trainSite/school/views.py
@@ -2,31 +2,67 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets, mixins
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, action
 from school.models import *
 from school.serializers import *
+from rest_framework import status
+from rest_framework.response import Response
+
+from drf_yasg.utils import swagger_auto_schema
 
 
 class TeacherView(ModelViewSet):
     queryset = Teacher.objects.all()
+    permission_classes = (IsAuthenticated, )
     serializer_class = TeacherSerializer
+
+    @swagger_auto_schema(method='post', operation_description="Добавить учителя", operation_summary="Добавить учителя")
+    @action(detail=True, methods=['post'])
+    def create(self, request, *args, **kwargs):
+        pass
+
+    @swagger_auto_schema(method='get', operation_description="Вывести учителей учителей", operation_summary="Вывести учителей учителей")
+    @action(detail=True, methods=['get'])
+    def list(self, request, *args, **kwargs):
+        pass
+
+    @swagger_auto_schema(method='delete', operation_description="Удалить учителя", operation_summary="Удалить учителя")
+    @action(detail=True, methods=['delete'])
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    @swagger_auto_schema(method='put', operation_description="Обновить информацию о учителе", operation_summary="Обновить информацию о учителе")
+    @action(detail=True, methods=['put'])
+    def update(self, request, *args, **kwargs):
+        pass
+
+    @swagger_auto_schema(method='get', operation_description="Информация о учителе", operation_summary="Информация о учителе")
+    @action(detail=True, methods=['get'])
+    def retrieve(self, request, *args, **kwargs):
+        pass
 
 
 class SubjectView(ModelViewSet):
+    """Работа с предметами"""
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class StudentView(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class StatementView(ModelViewSet):
     queryset = Statement.objects.all()
     serializer_class = StatementSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class OfficesView(ModelViewSet):
     queryset = Offices.objects.all()
     serializer_class = OfficesSerializer
+    permission_classes = (IsAuthenticated, )
--- trainSite/trainSite/yasg.py
+++ trainSite/trainSite/yasg.py
@@ -6,9 +6,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Django School",
+        title="School API",
         default_version='v1',
-        description="Test description",
+        description="Test API Project School",
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -16,8 +16,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger(P<format>\.json|\.yaml)', schema_view.without_ui(
-        cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger',
                                          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
