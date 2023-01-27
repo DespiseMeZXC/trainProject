8f8c16ce16343de4242a0fd824b26223769d5450
Taske <2003lesha2003@mail.ru>
2023-01-27 08:04:45 +0300

    some little changes

--- trainSite/trainSite/settings.py
+++ trainSite/trainSite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'school',
         'USER': 'postgres',
-        'PASSWORD': '24294352Pride!',
+        'PASSWORD': '2213',
         'HOST': 'localhost',
         'PORT': '5432',
     }
